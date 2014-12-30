a6ebb40cbc97c5d6747c181b55e23814d25f9558
Luiz Arantes Sa <LArantesSa@icloud.com>
2014-12-30 17:36:30 -0600

    Minify JavaScript

--- js/analytics.js
+++ js/analytics.js
@@ -1,15 +1,1 @@
-/**
- * Created by Kernel on 12/30/14.
- */
-window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-ga('create', 'UA-58074685-1', 'auto');
-ga('send', 'pageview');
-
-// Navigation Timing
-window.addEventListener("load", function(event) {
-  var t = window.performance.timing,
-    dcl = t.domContentLoadedEventStart - t.domLoading,
-    complete = t.domComplete - t.domLoading;
-  var stats = document.getElementById("crp-stats");
-  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
-});
+window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+(new Date);ga("create","UA-58074685-1","auto");ga("send","pageview");window.addEventListener("load",function(e){var t=window.performance.timing,n=t.domContentLoadedEventStart-t.domLoading,r=t.domComplete-t.domLoading;var i=document.getElementById("crp-stats");i.textContent="DCL: "+n+"ms, onload: "+r+"ms"})
